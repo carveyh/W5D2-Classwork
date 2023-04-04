515ef8a5fc9889bb3c21ac9b29ed65bff4833a6d
carveyh <carvey.hor@gmail.com>
2023-04-04 17:02:01 -0400

    Finish phase 06

--- skeleton/lib/06_joins_warmup.rb
+++ skeleton/lib/06_joins_warmup.rb
@@ -76,18 +76,36 @@ end
 def films_by_id
   # What are the titles of the films with id 1119, 1595, 1768?
   execute(<<-SQL)
+    SELECT
+      title
+    FROM
+      movies
+    WHERE
+      id IN (1119, 1595, 1768);
   SQL
 end
 
 def glenn_close_id
   # What id number does the actress 'Glenn Close' have?
   execute(<<-SQL)
+    SELECT
+      id
+    FROM
+      actors
+    WHERE
+      name = 'Glenn Close';
   SQL
 end
 
 def casablanca_id
   # What is the id of the film 'Casablanca'?
   execute(<<-SQL)
+    SELECT
+      id
+    FROM
+      movies
+    WHERE
+      title = 'Casablanca';
   SQL
 end
 
@@ -95,11 +113,41 @@ def casablanca_cast
   # Obtain the cast list for 'Casablanca'. Use the id value that you obtained
   # in the previous question directly in your query (for example, id = 1).
   execute(<<-SQL)
+    SELECT
+      name
+    FROM
+      actors
+    JOIN
+      castings ON castings.actor_id = actors.id
+    WHERE
+      movie_id = (
+        SELECT
+          id
+        FROM
+          movies
+        WHERE
+          title = 'Casablanca'
+      );
   SQL
 end
 
 def alien_cast
   # Obtain the cast list for the film 'Alien'
   execute(<<-SQL)
+    SELECT
+      name
+    FROM
+      actors
+    JOIN
+      castings ON castings.actor_id = actors.id
+    WHERE
+      movie_id = (
+        SELECT
+          id
+        FROM
+          movies
+        WHERE
+          title = 'Alien'
+      );
   SQL
 end
