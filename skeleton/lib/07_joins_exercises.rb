f60b3412c172aee90ec224e932eff8695f5297d6
root <somra45@gmail.com>
2023-04-04 16:03:52 -0400

    start phase 07

--- skeleton/lib/07_joins_exercises.rb
+++ skeleton/lib/07_joins_exercises.rb
@@ -40,20 +40,77 @@ end
 def ford_films
   # List the films in which 'Harrison Ford' has appeared.
   execute(<<-SQL)
+    SELECT
+      title
+    FROM
+      movies
+    JOIN
+      castings ON movies.id = castings.movie_id
+    JOIN
+      actors ON actors.id = castings.actor_id
+    WHERE
+      name = 'Harrison Ford';
   SQL
 end
+
+# def ford_films
+#   # List the films in which 'Harrison Ford' has appeared.
+#   execute(<<-SQL)
+#     SELECT
+#       title
+#     FROM
+#       movies
+#     JOIN
+#       castings ON movies.id = castings.movie_id
+#     WHERE
+#       actor_id = (
+#         SELECT
+#           id
+#         FROM
+#           actors
+#         WHERE
+#           name = 'Harrison Ford'
+#       );
+#   SQL
+# end
 
 def ford_supporting_films
   # List the films where 'Harrison Ford' has appeared - but not in the star
   # role. [Note: the ord field of casting gives the position of the actor. If
   # ord=1 then this actor is in the starring role]
   execute(<<-SQL)
+    SELECT
+      title
+    FROM 
+      movies
+    JOIN
+      castings ON castings.movie_id = movies.id
+    WHERE
+      actor_id = (
+        SELECT
+          id
+        FROM 
+          actors
+        WHERE
+          name = 'Harrison Ford'
+      ) AND ord != 1; -- refactor using group by
   SQL
 end
 
 def films_and_stars_from_sixty_two
   # List the title and leading star of every 1962 film.
   execute(<<-SQL)
+    SELECT
+      title, name
+    FROM 
+      movies
+    JOIN
+      castings ON movies.id = castings.movie_id
+    JOIN
+      actors On actors.id = castings.actor_id
+    WHERE
+      yr = 1962 AND ord = 1;
+
   SQL
 end
 
@@ -61,6 +118,20 @@ def travoltas_busiest_years
   # Which were the busiest years for 'John Travolta'? Show the year and the
   # number of movies he made for any year in which he made at least 2 movies.
   execute(<<-SQL)
+    SELECT
+      yr, COUNT(*)
+    FROM
+      movies
+    JOIN
+      castings ON movies.id = castings.movie_id
+    JOIN
+      actors On actors.id = castings.actor_id
+    WHERE
+      name = 'John Travolta'
+    GROUP BY
+      yr
+    HAVING
+      COUNT(*) >= 2;
   SQL
 end
 
